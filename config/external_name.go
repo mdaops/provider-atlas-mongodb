5e12edc89674b4dace31ab3fc518c9d563d9e66d
michael <michael@mabbott.dev>
2025-06-18 12:07:18 +0100

    chore(resources): appropriate external name configuration

--- config/external_name.go
+++ config/external_name.go
@@ -2,17 +2,12 @@ package config
 
 import "github.com/crossplane/upjet/pkg/config"
 
-// ExternalNameConfigs contains all external name configurations for this
-// provider.
 var ExternalNameConfigs = map[string]config.ExternalName{
 	"mongodbatlas_project":          config.IdentifierFromProvider,
-	"mongodbatlas_advanced_cluster": config.TemplatedStringAsIdentifier("", "{{.parameters.project_id}}-{{.external_name}}"),
-	"mongodbatlas_project_api_key":  config.TemplatedStringAsIdentifier("", "{{.parameters.project_id}}-{{.external_name}}"),
+	"mongodbatlas_advanced_cluster": config.IdentifierFromProvider,
+	"mongodbatlas_project_api_key":  config.IdentifierFromProvider,
 }
 
-// ExternalNameConfigurations applies all external name configs listed in the
-// table ExternalNameConfigs and sets the version of those resources to v1beta1
-// assuming they will be tested.
 func ExternalNameConfigurations() config.ResourceOption {
 	return func(r *config.Resource) {
 		if e, ok := ExternalNameConfigs[r.Name]; ok {
@@ -21,8 +16,6 @@ func ExternalNameConfigurations() config.ResourceOption {
 	}
 }
 
-// ExternalNameConfigured returns the list of all resources whose external name
-// is configured manually.
 func ExternalNameConfigured() []string {
 	l := make([]string, len(ExternalNameConfigs))
 	i := 0
